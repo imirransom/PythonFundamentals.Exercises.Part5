461e8dd6600c64606f9b9cad9ded1285571e43f5
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-10 05:19:37 -0500

    finished'

--- list_utils.py
+++ list_utils.py
@@ -12,7 +12,6 @@ def get_item_at_position(list_in: List, pos: int) -> List:
     """
     return list_in[pos]
 
-
 def print_list_items(list_in: List) -> None:
     """
     Given a list, this function iterates through it and prints each element.
@@ -31,7 +30,8 @@ def sort_by_commit_count(list_in: List) -> List:
     :param list_in: A list where each entry is a list containing a name and the commit count corresponding to a user
     :return: The same list sorted in ascending order based on the commit count
     """
-    return sorted(list_in)
+    list_in.sort(key = lambda list_in: list_in[1])
+    return list_in
 
 
 def gen_list_of_nums(n: int) -> List[int]:
